61e18153658e02c3fd85a6b32587e26406414499
David Wilson <wilson11235813@gmail.com>
2020-02-29 23:59:27 -0600

    te

--- CSV_Tutorial/7_main.py
+++ CSV_Tutorial/7_main.py
@@ -18,5 +18,5 @@ row_dict = {'Org': 'St Louis', 'Task': 'Query','Time Spent': 56}
  
  
 # Append a dict as a row in csv file
-append_dict_as_row('Hours.csv', row_dict, field_names)
+append_dict_as_row('Hours.csv', row_dict, field_names)7
         
