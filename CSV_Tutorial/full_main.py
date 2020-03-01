3c2daf07c1d38287c9bac96043572274f917785f
David Wilson <wilson11235813@gmail.com>
2020-02-29 23:14:01 -0600

    finalized full_main.py to append the fields and data I want.

--- CSV_Tutorial/full_main.py
+++ CSV_Tutorial/full_main.py
@@ -1,49 +1,42 @@
 from csv import writer
+# This appends a row of dictionary information about how you spent your time working to a csv.
+# It will create the csv if it doesnt already exist. 
+
 from csv import DictWriter
 
-def append_list_as_row(file_name, list_of_elem):
-    with open(file_name, 'a+', newline='') as write_obj:
-        csv_writer = writer(write_obj)
-        csv_writer.writerow(list_of_elem)
 
-def append_dict_as_row(file_name, dict_of_elem, field_names):
-    with open(file_name, 'a+', newline = '') as write_obj:
-        dict_writer = DictWriter(write_obj, fieldnames=field_names)
-        dict_writer.writerow(dict_of_elem)
+def append_dict_as_row(csv_file, hours_data, fields):
+    try:
+        with open(csv_file, 'a+', newline = '') as csvfile:
+            writer = DictWriter(csvfile, fieldnames=fields)
+            writer.writeheader()
+            for data in hours_data:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
 
 def main():
-    print('*** Append new row to an existing csv file using csv.writer() in python ***')
+    print('Appending dictionary entries to csv.')
  
-    # List of strings
-    row_contents = [32,'Shaun','Java','Tokyo','Morning']
- 
-    # Append a list as new line to an old csv file
-    append_list_as_row('students.csv', row_contents)
- 
-    print('** Append new row to an existing csv file with less items using csv.writer()  in python **')
- 
-    # A list with missing entries
-    row_contents = [33, 'Sahil', 'Morning']
- 
-    # Appending a row to csv with missing entries
-    append_list_as_row('students.csv', row_contents)
- 
-    print('Append dictionary as a row to an existing csv file using DictWriter in python')
- 
-    field_names = ['Id','Name','Course','City','Session']
-    row_dict = {'Id': 81,'Name': 'Sachin','Course':'Maths','City':'Mumbai','Session':'Evening'}
- 
-    # Append a dict as a row in csv file
-    append_dict_as_row('students.csv', row_dict, field_names)
+    fields = ['Org','Task','Time Spent']
+    hours_data = [
+    {'Org': 'ARC', 'Task': 'Files', 'Time Spent': 14},
+    {'Org': 'Tampa', 'Task': 'Call', 'Time Spent': 4},
+    {'Org': 'USO', 'Task': 'Query', 'Time Spent': 7},
+    {'Org': 'LLS', 'Task': 'Call', 'Time Spent': 78},
+    {'Org': 'Tampa', 'Task': 'Call', 'Time Spent': 43},
+    ]
+    # Append a dict as a row in csv file, or create the csv first.
+    append_dict_as_row('hours.csv', hours_data, fields)
  
     print('Append a dictionary with missing entries as a new row to an existing csv file using DictWriter**')
  
     # If empty entries are missed then DictWriter will handle them automatically
-    field_names = ['Id','Name','Course','City','Session']
-    row_dict = {'Id': 33, 'Name':'Eva', 'Session':'Evening'}
+    fields = ['Org','Task','Time Spent']
+    hours_data = [{'Task':'MC Update','Time Spent': 30}]
  
     # Append a dict missing entries as a row in csv file
-    append_dict_as_row('students.csv', row_dict, field_names)
+    append_dict_as_row('hours.csv', hours_data, fields)
  
  
 if __name__ == '__main__':
