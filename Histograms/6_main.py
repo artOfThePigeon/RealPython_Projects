ca46123fe8f9a4793f023b3e61d10ecb1c9396c8
david wilson <wilson11235813@gmail.com>
2020-03-05 22:32:51 -0600

    added figure

--- Histograms/6_main.py
+++ Histograms/6_main.py
@@ -1,4 +1,4 @@
-#presents data in a histogram using a pandas series
+# Histogram & kernel density estimates 
 
 import numpy as np
 import matplotlib.pyplot as plt
@@ -8,8 +8,17 @@ means = 10, 20
 stdevs = 4, 2
 
 dist = pd.DataFrame(
-    np.random.normal(loc=means, scale=stdevs, size=(1000,2))
+    np.random.normal(loc=means, scale=stdevs, size=(1000,2)),
     columns=['a','b']
 )
 
 print(dist.agg(['min','max','mean','std']))
+
+fig, ax = plt.subplots()
+dist.plot.kde(ax=ax, legend=False, title='Histogram: A vs. B')
+dist.plot.hist(density=True, ax=ax)
+ax.set_ylabel('Probability')
+ax.grid(axis='y')
+ax.set_facecolor('#d8dcd6')
+
+plt.show()
