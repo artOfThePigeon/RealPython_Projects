e36a144b9b72b51561b594a76d73e05c6e42512b
david wilson <wilson11235813@gmail.com>
2020-03-07 22:07:13 -0600

    added code for smoothed out kde

---
+++ Histograms/8_main.py
@@ -0,0 +1,16 @@
+#basic histogram using seaborn, with a smoothed out kde using scipy
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from scipy import stats
+
+np.random.seed(444)
+
+d = np.random.laplace(loc=15, scale=3, size=500)
+
+sns.set_style('darkgrid')
+sns.distplot(d, fit=stats.laplace, kde=False)
+
+plt.show()
