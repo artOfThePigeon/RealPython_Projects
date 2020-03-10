dcf93fa978e07e23a0a577b325705b38a6fd0ce3
david wilson <wilson11235813@gmail.com>
2020-03-09 22:37:26 -0500

    pandas tools exercise

---
+++ Histograms/10_main.py
@@ -0,0 +1,15 @@
+#using pandas cut() method to bin values into sensible intervals
+
+import numpy as np
+import pandas as pd
+
+ages = pd.Series(
+    [1, 1, 3, 5, 8, 10, 12, 15, 18, 18, 19, 20, 25, 30, 40, 51, 52]
+)
+
+bins = [0, 10, 13, 18, 21, np.inf]
+labels = ['child', 'preteen', 'teen', 'military age', 'adult']
+
+groups = pd.cut(ages, bins=bins, labels=labels)
+
+print(groups.value_counts())
