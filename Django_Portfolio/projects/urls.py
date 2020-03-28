a748ce619ad2ae2212091f772cfe05ff97934dea
david wilson <wilson11235813@gmail.com>
2020-03-27 23:36:28 -0500

    fixed no reverse match error by coding in urls

--- Django_Portfolio/projects/urls.py
+++ Django_Portfolio/projects/urls.py
@@ -2,9 +2,11 @@ from django.contrib import admin
 from django.urls import include, path
 from projects import views
 
+app_name = 'projects '
+
 urlpatterns = [
-    path('', views.all_projects),
+    path('', views.all_projects  name='all_projects'),
     path('po', views.project_list),
-    path('<int:pk>', views.project_detail)
+    path('<int:pk>', views.project_detail, name='project_detail')
 
 ]
