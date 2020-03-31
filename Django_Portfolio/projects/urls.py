ba3c853a9220acad262d784f615697e5d4b8b007
david wilson <wilson11235813@gmail.com>
2020-03-30 23:32:59 -0500

    added more styling, made mobile responsive, registered Project model in admin interface

--- Django_Portfolio/projects/urls.py
+++ Django_Portfolio/projects/urls.py
@@ -5,8 +5,7 @@ from projects import views
 app_name = 'projects '
 
 urlpatterns = [
-    path('', views.all_projects  name='all_projects'),
+    path('', views.all_projects, name='all_projects'),
     path('po', views.project_list),
     path('<int:pk>', views.project_detail, name='project_detail')
-
 ]
