e635f5a1d667834b784af9e765146ef4b7ea7c36
David Wilson <wilson11235813@gmail.com>
2020-04-13 22:45:35 -0500

    merge sort algorithm

--- Algorithms/2_main.py
+++ Algorithms/2_main.py
@@ -1,3 +1,6 @@
+from random import randint
+from timeit import repeat
+
 def bubble_sort(array):
     n = len(array)
 
@@ -26,4 +29,28 @@ def bubble_sort(array):
             break
 
     return array
+
+def run_sorting_algorithm(algorithm, array):
+    setup_code = f"from __main__ import {algorithm}" \
+                 if algorithm != "sorted" else ""
+
+    stmt = f"{algorithm}({array})"
+
+    # Execute the code ten different times and return the time in seconds that each execution took
+    times = repeat(setup=setup_code, stmt=stmt, repeat=3, number=10)
+
+    # display the name of the algorithm and the minimum time it took to run
+    print(f"Algorithm: {algorithm}. Minimum execution time: {min(times)}")
+
+
+ARRAY_LENGTH = 10000
+
+if __name__ == "__main__":
+    # generate an array of ARRAY_LENGTH items consisting of random integer values
+    # between 0 and 999
+    array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+
+    # call the function using the name of the sorting algorithm and the array you just created
+    run_sorting_algorithm(algorithm="bubble_sort", array=array)   
+
             
--- Algorithms/3_main.py
+++ Algorithms/3_main.py
@@ -1,3 +1,6 @@
+from random import randint
+from timeit import repeat
+
 def insertion_sort(array):
     #loop from the second element of the array until the last element
     for i in range(1, len(array)):
@@ -23,4 +26,27 @@ def insertion_sort(array):
 
     return array
 
+def run_sorting_algorithm(algorithm, array):
+    setup_code = f"from __main__ import {algorithm}" \
+                 if algorithm != "sorted" else ""
+
+    stmt = f"{algorithm}({array})"
+
+    # Execute the code ten different times and return the time in seconds that each execution took
+    times = repeat(setup=setup_code, stmt=stmt, repeat=3, number=10)
+
+    # display the name of the algorithm and the minimum time it took to run
+    print(f"Algorithm: {algorithm}. Minimum execution time: {min(times)}")
+
+
+ARRAY_LENGTH = 10000
+
+if __name__ == "__main__":
+    # generate an array of ARRAY_LENGTH items consisting of random integer values
+    # between 0 and 999
+    array = [randint(0, 1000) for i in range(ARRAY_LENGTH)]
+
+    # call the function using the name of the sorting algorithm and the array you just created
+    run_sorting_algorithm(algorithm="insertion_sort", array=array)   
     
+
